7b44eca84c7ec3fcdb4a56596f0678894f60a893
Geoffrey28 <geoffreygirard28@gmail.com>
2024-07-04 17:31:44 +0200

    commit

--- 2 - API_TMDB/js/showMovies.js
+++ 2 - API_TMDB/js/showMovies.js
@@ -3,12 +3,13 @@ const moviesList = document.querySelector('#movies-list');
 export function showMovies(movies) {
     for (let movie of movies) {
         console.log(movie);
-        let img = document.createElement('img');
-        img.srcset = movie.poster_path;
-        let li = document.createElement('li');
-        li.classList.add('list-group-item', 'w-25');
-        li.textContent = movie.title;
-        moviesList.append(img);
-        moviesList.append(li);
+        let item = document.createElement('li');
+        item.classList.add('list-group-item', 'w-25');
+        let title = document.createElement('h3');
+        title.textContent = movie.title;
+        let description = document.createElement('p');
+        description.textContent = movie.overview;
+        item.append(title, description);
+        moviesList.append(item);
     }
 }
